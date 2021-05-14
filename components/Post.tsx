84287bcc037086828147dbc60ec61f855e9e8d66
nabesuke <nabesuke5098@gmail.com>
2021-05-14 09:19:04 +0900

    [add]Post component

---
+++ components/Post.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import { POST } from '../types/Types'
+
+const Post: React.FC<POST> = ({ id, title }) => {
+  return (
+    <div>
+      <span>{id}</span>
+      {' : '}
+      <Link href={`/posts/${id}`}>
+        <a className="cursor-pointer border-gray-500 hover:bg-gray-300">
+          {title}
+        </a>
+      </Link>
+    </div>
+  )
+}
