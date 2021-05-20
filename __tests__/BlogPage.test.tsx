49f0215c0a2d6b82a5ced759b6dde457b3ea074d
nabesuke <nabesuke5098@gmail.com>
2021-05-21 07:58:09 +0900

    [modified]test MSW warning

--- __tests__/BlogPage.test.tsx
+++ __tests__/BlogPage.test.tsx
@@ -8,9 +8,10 @@ import { setupServer } from 'msw/node'
 initTestHelpers()
 
 const handlers = [
-  rest.get(
-    'https://jsonplaceholder.typicode.com/posts/?_limit=10',
-    (req, res, ctx) => {
+  rest.get('https://jsonplaceholder.typicode.com/posts/', (req, res, ctx) => {
+    const query = req.url.searchParams
+    const _limit = query.get('_limit')
+    if (_limit === '10') {
       return res(
         ctx.status(200),
         ctx.json([
@@ -29,7 +30,7 @@ const handlers = [
         ])
       )
     }
-  ),
+  }),
 ]
 
 const server = setupServer(...handlers)
